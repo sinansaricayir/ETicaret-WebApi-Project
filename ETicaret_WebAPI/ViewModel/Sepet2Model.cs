30fc7a14bc156b8730e7537aab4e41f02baea02e
Sinan Sarıçayır <sinansaricayir@gmail.com>
2021-06-25 00:24:42 +0300

    Final
    Check

---
+++ ETicaret_WebAPI/ViewModel/Sepet2Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ETicaret_WebAPI.ViewModel
+{
+    public class Sepet2Model
+    {
+        public int SepetId { get; set; }
+        public int SepetUrunId { get; set; }
+        public UrunModel urunBilgi { get; set; }
+        public int SepetUyeId { get; set; }
+        public MusteriModel musteriBilgi { get; set; }
+        public int SepetOnay { get; set; }
+    }
+}
---
+++ ETicaret_WebAPI/ViewModel/YoneticiModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ETicaret_WebAPI.ViewModel
+{
+    public class YoneticiModel
+    {
+        public int yoneticiId { get; set; }
+        public string yoneticiAd { get; set; }
+        public string yoneticiSifre { get; set; }
+
+        public string yoneticiYetki { get; set; }
+    }
+}
